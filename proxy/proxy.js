a060f45ff3d785a046094f196bfd17733cd72921
Rapha-0 <raphael.wiggert@gmx.de>
2025-01-10 12:55:58 +0100

    Add frontend to docker

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Configure proxy endpoints
 app.use("/jaeger", proxy("http://jaeger:16686")); 
-app.use("/", proxy("http://frontend:80"));
+app.use("/", proxy("http://collekt-frontend:80"));
 
 // Create and start both http and https server to handle all incoming traffic
 const httpServer = http.createServer(app);
