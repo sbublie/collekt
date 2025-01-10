3986cd16dc2b967afe9231972b0ff2a51c2a8675
sbublie <59267927+sbublie@users.noreply.github.com>
2025-01-10 09:18:10 +0100

    Add proxy

---
+++ proxy/proxy.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const proxy = require("express-http-proxy");
+const app = express();
+
+
+// Configure proxy endpoints
+app.use("/jaeger", proxy("http://jaeger:80")); 
+app.use("/", proxy("http://frontend:80"));
+
+// Create and start both http and https server to handle all incoming traffic
+const httpServer = http.createServer(app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server started");
+});
