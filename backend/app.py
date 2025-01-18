cba9a4300e9ed53c0f9435e67c87fd0d7f5fd28b
sbublie <59267927+sbublie@users.noreply.github.com>
2025-01-18 11:04:51 +0100

    Add auto instrumentalisation

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import time
-from opentelemetry import trace
+from typing import List, Optional
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
-from typing import List, Optional
+from opentelemetry import trace
+
+# Define the service name
+resource = Resource.create({"service.name": "collekt"})
+
+# Set up the Tracer Provider
+provider = TracerProvider(resource=resource)
+
+# Configure OTLP Exporter (sends traces to Jaeger)
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+
+# Add the OTLP exporter to the TracerProvider
+span_processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(span_processor)
+
+# Set the global TracerProvider
+trace.set_tracer_provider(provider)
 
 app = FastAPI(
     title="Collekt Backend API",
@@ -15,22 +31,8 @@ app = FastAPI(
     version="0.0.1"
 )
 
-# Define the service name
-resource = Resource.create({"service.name": "collekt"})
-
-# Step 1: Set up the Tracer Provider
-provider = TracerProvider(resource=resource)
-trace.set_tracer_provider(provider)
-
-# Step 2: Configure OTLP Exporter (sends traces to Jaeger)
-otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
-
-# Step 3: Add the OTLP exporter to the TracerProvider
-span_processor = BatchSpanProcessor(otlp_exporter)
-provider.add_span_processor(span_processor)
-
-# Get a tracer
-tracer = trace.get_tracer("collekt")
+# Automatically instrument FastAPI
+FastAPIInstrumentor.instrument_app(app)
 
 class Offerings(BaseModel):
     wishlist_item_id: str
@@ -48,70 +50,42 @@ class WishlistItem(BaseModel):
 class Wishlist(BaseModel):
     items: List[WishlistItem] = []
 
-# In-Memory-Datenbank
+# In-Memory-Database
+# TODO: Replace with a real database
 wishlist = Wishlist(items=[])
 
 @app.get("/wishlist", response_model=Wishlist)
 async def get_wishlist():
-    with tracer.start_as_current_span("get-wishlist-from-backend") as parent:
-        parent.set_attribute("component", "frontend")
-        time.sleep(0.12446)
-        with tracer.start_as_current_span("request-wishlist-from-db") as child:
-            child.set_attribute("component", "backend")
-            time.sleep(0.132541)  # Simulating database fetch delay
-            with tracer.start_as_current_span("db-processing") as child:
-                child.set_attribute("component", "db")
-                time.sleep(0.32541) 
-            time.sleep(0.14446)
-        time.sleep(0.14446)
-        return wishlist
+    return wishlist
 
 @app.get("/wishlist/{item_id}", response_model=WishlistItem)
 async def get_wishlist_item(item_id: str):
-    with tracer.start_as_current_span("get-wishlist-item") as parent:
-        parent.set_attribute("custom-tag", "get-wishlist-item")
-
-        for item in wishlist.items:
-            if item.id == item_id:
-                return item
-        raise HTTPException(status_code=404, detail="Item not found")
+    for item in wishlist.items:
+        if item.id == item_id:
+            return item
+    raise HTTPException(status_code=404, detail="Item not found")
 
 @app.post("/wishlist", response_model=WishlistItem)
 async def add_item_to_wishlist(item: WishlistItem):
-    with tracer.start_as_current_span("add-item-to-wishlist") as parent:
-        parent.set_attribute("custom-tag", "add-wishlist-item")
-        
-        # Check if the item already exists
-        for existing_item in wishlist.items:
-            if existing_item.id == item.id:
-                raise HTTPException(status_code=400, detail="Item with this ID already exists")
-        
-        with tracer.start_as_current_span("add-item-to-db") as child:
-            child.set_attribute("operation", "posting wishlist item")
-            time.sleep(0.32541)  # Simulating database fetch delay
-        
-        # Add the item to the wishlist
-        wishlist.items.append(item)
-        
-        # Return the newly added item
-        return item
-
+    # Check if the item already exists
+    for existing_item in wishlist.items:
+        if existing_item.id == item.id:
+            raise HTTPException(status_code=400, detail="Item with this ID already exists")
+    wishlist.items.append(item)
+    return item
 
 @app.delete("/wishlist/{item_id}")
 async def delete_wishlist_item(item_id: str):
-    with tracer.start_as_current_span("delete-wishlist-item") as parent:
-        parent.set_attribute("custom-tag", "delete-wishlist-item")
-        for index, item in enumerate(wishlist.items):
-            if item.id == item_id:
-                del wishlist.items[index]
-                return {"message": "Item deleted"}
-        raise HTTPException(status_code=404, detail="Item not found")
+    for index, item in enumerate(wishlist.items):
+        if item.id == item_id:
+            del wishlist.items[index]
+            return {"message": "Item deleted"}
+    raise HTTPException(status_code=404, detail="Item not found")
 
 @app.get("/offerings/{item_id}", response_model=List[Offerings])
 async def get_offerings(item_id: str) -> List[Offerings]:
-    with tracer.start_as_current_span("get-offerings") as parent:
-        parent.set_attribute("custom-tag", "get-offerings")
-        for item in wishlist.items:
-            if item.id == item_id:
-                return [{"wishlist_item_id": item_id, "title": "1989 (TAYLORS VERSION) CRYSTAL SKIES BLUE CD", "price": 19.97, "url": "https://www.amazon.de/1989-Taylors-Version-Chrystal-Skies/dp/B0CFM76QSG/ref=sr_1_4_mod_primary_new?crid=2LWIL08LAEBML&dib=eyJ2IjoiMSJ9.SUMq_X88Q5YtKX8rU5T1905PUZIQ1dR8lueYhSD4eIFpPYuYdlqJL9L4dPM3XSBD8jniA0t_PcwlFoTsg6dwnU_8hgpTvRsxIBYMDtOJC-syAt7TLd2CIPKAOmavlJjndlS3hOVQ5WTkzQ6vBks1dAVHp1jl1b_U1UtEnVw9339bHjm8PzsGdYHXXTYTe4rEI4dzmeMWbFO2RV0A6X2el2_f9Agxz3uwYzJp5IiV3VY.-OUjXQQGIh8cY1ZbMSMp1M4UmXSWtyC95rBiIohFjX0&dib_tag=se"}]
-        raise HTTPException(status_code=404, detail="Item not found or no offerings available")
+    for item in wishlist.items:
+        if item.id == item_id:
+            # Please note that this is a mock implementation
+            return [{"wishlist_item_id": item_id, "title": "1989 (TAYLORS VERSION) CRYSTAL SKIES BLUE CD", "price": 19.97, "url": "https://www.amazon.de/1989-Taylors-Version-Chrystal-Skies/dp/B0CFM76QSG"}]
+    raise HTTPException(status_code=404, detail="Item not found or no offerings available")
